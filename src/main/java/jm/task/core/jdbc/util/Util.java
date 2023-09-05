e2474b836fed786a89888f98faf472eb4586f574
Роман <savinr95@gmail.com>
2023-09-05 21:31:29 +0300

    fix: "перенес подключение"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,24 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     public static Connection getMyConnection() throws SQLException,
             ClassNotFoundException {
-        return MySQLConnUtils.getMySQLConnection();
+        String hostName = "localhost";
+        String dbName = "home";
+        String userName = "root";
+        String password = "root";
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) throws SQLException,
+            ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        return DriverManager.getConnection(connectionURL, userName, password);
     }
 }
