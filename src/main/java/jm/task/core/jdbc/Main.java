01257239386cf68e66eb78c4ba2ea9b1a76c30e9
Роман <savinr95@gmail.com>
2023-09-05 21:28:59 +0300

    fix: "реализовал тз"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,16 +1,24 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.util.MySQLConnUtils;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        System.out.println("Get connection ... ");
-        // Get a Connection object
-        Connection conn = MySQLConnUtils.getMySQLConnection();
-        System.out.println("Get connection " + conn);
-        System.out.println("Done!");
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Володя", "Володин", (byte) 20);
+        System.out.println("User с именем - Володя добавлен в базу данных ");
+        userService.saveUser("Володя1", "Володин1", (byte) 21);
+        System.out.println("User с именем - Володя1 добавлен в базу данных ");
+        userService.saveUser("Володя2", "Володин2", (byte) 22);
+        System.out.println("User с именем - Володя2 добавлен в базу данных ");
+        userService.saveUser("Володя3", "Володин3", (byte) 23);
+        System.out.println("User с именем - Володя3 добавлен в базу данных ");
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
