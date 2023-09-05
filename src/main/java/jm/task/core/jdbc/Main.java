934a48513fc2ea22b94d8b5aaf5b29d075cd6a2d
Роман <savinr95@gmail.com>
2023-09-05 18:45:11 +0300

    feat: "Настроил подключение"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,16 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.MySQLConnUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        System.out.println("Get connection ... ");
+        // Get a Connection object
+        Connection conn = MySQLConnUtils.getMySQLConnection();
+        System.out.println("Get connection " + conn);
+        System.out.println("Done!");
     }
 }
